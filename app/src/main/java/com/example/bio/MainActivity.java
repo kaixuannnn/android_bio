f29c78c4bdfae163a01ac8843b6ecbc016a87ee8
Kai Xuan <kaixuan@agmostudio.com>
2021-11-06 10:46:44 +0800

    second commit

--- app/src/main/java/com/example/bio/MainActivity.java
+++ app/src/main/java/com/example/bio/MainActivity.java
@@ -7,8 +7,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.example.bio.databinding.ActivityMainBinding;
 
